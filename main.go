71d7c68adbac2b7720f3d628cfa8f8dbf33bb35c
Dawson Hensel <dawsonhensel@gmail.com>
2023-11-30 20:12:28 -0800

    setup hello world for templ

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/a-h/templ"
+)
+
 func main() {
+	component := hello("John")
+
+	http.Handle("/", templ.Handler(component))
+
+	fmt.Println("Listening on localhost:3000")
+	http.ListenAndServe("localhost:3000", nil)
 
 }
