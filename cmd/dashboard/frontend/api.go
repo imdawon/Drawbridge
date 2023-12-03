752e956df8a91c5229f85c9a1c7c3e04c2f6dc58
Dawson Hensel <dawsonhensel@gmail.com>
2023-12-02 16:28:13 -0800

    create new service

--- cmd/dashboard/frontend/api.go
+++ cmd/dashboard/frontend/api.go
@@ -4,16 +4,20 @@ import (
 	"dhens/drawbridge/cmd/dashboard/frontend/templates"
 	"log"
 	"net/http"
-
-	"github.com/a-h/templ"
 )
 
 func SetUpAPI(hostAndPort string) {
 	log.Printf("Starting frontend api service on %s", hostAndPort)
-	http.Handle("/", http.FileServer(http.Dir("./static")))
+	// FOr testing, so we can access the html files we create
+	http.Handle("/", http.FileServer(http.Dir("./cmd/dashboard")))
 
-	component := templates.Hello("John")
-	http.Handle("/api/new-service", templ.Handler(component))
+	http.HandleFunc("/new-service", func(w http.ResponseWriter, r *http.Request) {
+		templates.Hello(r).Render(r.Context(), w)
+	})
 
 	log.Fatal(http.ListenAndServe(hostAndPort, nil))
+}
+
+func handleCreateNewService(r *http.Request) {
+
 }
--- cmd/dashboard/frontend/templates/hello_templ.go
+++ cmd/dashboard/frontend/templates/hello_templ.go
@@ -10,7 +10,9 @@ import "context"
 import "io"
 import "bytes"
 
-func Hello(name string) templ.Component {
+import "net/http"
+
+func Hello(r *http.Request) templ.Component {
 	return templ.ComponentFunc(func(ctx context.Context, templ_7745c5c3_W io.Writer) (templ_7745c5c3_Err error) {
 		templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templ_7745c5c3_W.(*bytes.Buffer)
 		if !templ_7745c5c3_IsBuffer {
@@ -23,21 +25,45 @@ func Hello(name string) templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div>")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<li>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Var2 := `Hello, `
+		templ_7745c5c3_Var2 := `Name: `
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ_7745c5c3_Var2)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		var templ_7745c5c3_Var3 string = name
+		var templ_7745c5c3_Var3 string = r.PostFormValue("service-name")
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var3))
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</div>")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</li><li>")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		templ_7745c5c3_Var4 := `Host: `
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ_7745c5c3_Var4)
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		var templ_7745c5c3_Var5 string = r.PostFormValue("service-host")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var5))
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		templ_7745c5c3_Var6 := `:`
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ_7745c5c3_Var6)
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		var templ_7745c5c3_Var7 string = r.PostFormValue("service-port")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var7))
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</li>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
