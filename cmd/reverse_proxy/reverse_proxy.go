d2564ac959050b2953ce8c2d49a819bfced28dc9
Dawson Hensel <dawsonhensel@gmail.com>
2023-12-05 19:27:03 -0800

    Create Root CA and Server certificate

--- cmd/reverse_proxy/reverse_proxy.go
+++ cmd/reverse_proxy/reverse_proxy.go
@@ -1,50 +1,131 @@
 package proxy
 
 import (
+	"bytes"
+	"crypto/ed25519"
+	"crypto/rand"
 	"crypto/tls"
 	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
 	"fmt"
 	"log"
-	"net/http"
-	"os"
+	"math/big"
+	"time"
 )
-
-var (
-	CACertFilePath = "./contoso.crt"
-	CertFilePath   = "./server-crt.pem"
-	KeyFilePath    = "./server-key.pem"
-)
-
-func httpRequestHandler(w http.ResponseWriter, req *http.Request) {
-	w.Write([]byte("Hello,World!\n"))
-}
 
 func main() {
-	// load tls certificates
-	serverTLSCert, err := tls.LoadX509KeyPair(CertFilePath, KeyFilePath)
+	serverTLSConfig, clientTLSConfig, err := setupRootCA()
 	if err != nil {
-		log.Fatalf("Error loading certificate and key file: %v", err)
+		log.Fatalf("Error setting up root CA: %s", err)
 	}
-	// Configure the server to trust TLS client cert issued by your CA.
+	fmt.Printf("Server TLS Config: %v\nClient TLS Config: %v", serverTLSConfig, clientTLSConfig)
+}
+
+func setupRootCA() (serverTLSConfig *tls.Config, clientTLSConfig *tls.Config, err error) {
+	ca := &x509.Certificate{
+		SerialNumber: big.NewInt(2023),
+		Subject: pkix.Name{
+			Organization:  []string{"Drawbridge"},
+			Country:       []string{""},
+			Province:      []string{""},
+			Locality:      []string{""},
+			StreetAddress: []string{""},
+			PostalCode:    []string{""},
+		},
+		NotBefore:             time.Now(),
+		NotAfter:              time.Now().AddDate(10, 0, 0),
+		IsCA:                  true,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
+		KeyUsage:              x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
+		BasicConstraintsValid: true,
+	}
+
+	// Create our servers keypair
+	caPublicKey, caPrivateKey, err := ed25519.GenerateKey(rand.Reader)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// Create the CA
+	caBytes, err := x509.CreateCertificate(rand.Reader, ca, ca, &caPublicKey, caPrivateKey)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// PEM encode
+	caPEM := new(bytes.Buffer)
+	pem.Encode(caPEM, &pem.Block{
+		Type:  "CERTIFICATE",
+		Bytes: caBytes,
+	})
+
+	caPrivateKeyBytes, err := x509.MarshalPKCS8PrivateKey(caPrivateKey)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	caPrivateKeyPEM := new(bytes.Buffer)
+	pem.Encode(caPrivateKeyPEM, &pem.Block{
+		Type:  "OPENSSH PRIVATE KEY",
+		Bytes: caPrivateKeyBytes,
+	})
+
+	// Set up server certificate
+	cert := &x509.Certificate{
+		SerialNumber: big.NewInt(2023),
+		Subject: pkix.Name{
+			Organization:  []string{"Drawbridge"},
+			Country:       []string{""},
+			Province:      []string{""},
+			Locality:      []string{""},
+			StreetAddress: []string{""},
+			PostalCode:    []string{""},
+		},
+	}
+
+	// Create our certificate keypair
+	certPublicKey, certPrivateKey, err := ed25519.GenerateKey(rand.Reader)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	certBytes, err := x509.CreateCertificate(rand.Reader, cert, ca, &certPublicKey, certPrivateKey)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	certPrivateKeyBytes, err := x509.MarshalPKCS8PrivateKey(certPrivateKey)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	certPEM := new(bytes.Buffer)
+	pem.Encode(certPEM, &pem.Block{
+		Type:  "CERTIFICATE",
+		Bytes: certBytes,
+	})
+
+	certPrivateKeyPEM := new(bytes.Buffer)
+	pem.Encode(certPrivateKeyPEM, &pem.Block{
+		Type:  "OPENSSH PRIVATE KEY",
+		Bytes: certPrivateKeyBytes,
+	})
+
+	serverCert, err := tls.X509KeyPair(certPEM.Bytes(), certPrivateKeyPEM.Bytes())
+	if err != nil {
+		return nil, nil, err
+	}
+
+	serverTLSConfig = &tls.Config{
+		Certificates: []tls.Certificate{serverCert},
+	}
+
 	certPool := x509.NewCertPool()
-	if caCertPEM, err := os.ReadFile(CACertFilePath); err != nil {
-		panic(err)
-	} else if ok := certPool.AppendCertsFromPEM(caCertPEM); !ok {
-		panic("invalid cert in CA PEM")
+	certPool.AppendCertsFromPEM(caPEM.Bytes())
+	clientTLSConfig = &tls.Config{
+		RootCAs: certPool,
 	}
-	tlsConfig := &tls.Config{
-		ServerName:   "server.localhost",
-		ClientAuth:   tls.RequireAndVerifyClientCert,
-		ClientCAs:    certPool,
-		RootCAs:      certPool,
-		Certificates: []tls.Certificate{serverTLSCert},
-	}
-	server := http.Server{
-		Addr:      "localhost:443",
-		Handler:   http.HandlerFunc(httpRequestHandler),
-		TLSConfig: tlsConfig,
-	}
-	defer server.Close()
-	fmt.Printf("Server starting...")
-	log.Fatal(server.ListenAndServeTLS("", ""))
+
+	return
 }
