432cb2a07ea257d3d80a534fe5c105d3dc2ab578
Dawson Hensel <dawsonhensel@gmail.com>
2024-02-19 13:31:39 -0800

    Add back code removed during bad merge

--- cmd/dashboard/ui/api.go
+++ cmd/dashboard/ui/api.go
@@ -5,7 +5,6 @@ import (
 	"dhens/drawbridge/cmd/drawbridge"
 	"dhens/drawbridge/cmd/drawbridge/db"
 	proxy "dhens/drawbridge/cmd/reverse_proxy"
-	"fmt"
 	"log"
 	"log/slog"
 	"net/http"
@@ -24,8 +23,8 @@ type Controller struct {
 	Sql *db.SQLiteRepository
 }
 
-func (f *Controller) SetUp(hostAndPort string) error {
-	slog.Info(fmt.Sprintf("Starting frontend api service on %s", hostAndPort))
+func (f *Controller) SetUp(hostAndPort string, ca *certificates.CA) error {
+	log.Printf("Starting frontend api service on %s", hostAndPort)
 
 	// Start listener for all services from the db
 	services, err := f.Sql.GetAllServices()
