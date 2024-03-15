106add3cd39045e26588525dbb2743cf0a210a37
Dawson Hensel <dawsonhensel@gmail.com>
2024-03-15 08:56:39 -0700

    Use executable file path instead of terminal launching dir

--- cmd/utils/utils.go
+++ cmd/utils/utils.go
@@ -6,20 +6,31 @@ import (
 	"log"
 	"log/slog"
 	"os"
+	"path"
+	"path/filepath"
 )
 
 // Save file if it does not already exist.
 func SaveFile(fileName string, fileContents string, relativePath string) error {
-	fullFilePath := fmt.Sprintf("%s/%s", relativePath, fileName)
+	execPath, err := os.Executable()
+	if err != nil {
+		log.Fatal(err)
+	}
+	execDirPath := path.Dir(execPath)
+	relativePath = filepath.Join(execDirPath, relativePath)
+	fullFilePath := filepath.Join(relativePath, fileName)
+
 	// Verify the file doesn't exist before opening.
-	_, err := os.Open(fullFilePath)
+	_, err = os.Open(fullFilePath)
 	if !errors.Is(err, os.ErrNotExist) {
-		slog.Info(fmt.Sprintf("SAVE FILE: Skipping existing Drawbridge file: %s", fullFilePath))
+		slog.Debug(fmt.Sprintf("SAVE FILE: Skipping existing Drawbridge file: %s", fullFilePath))
 		return nil
 	}
 
 	// Create folder path if it doesn't exist.
 	if _, err := os.Stat(relativePath); errors.Is(err, os.ErrNotExist) {
+		slog.Debug("CREATING FOLDER PATH EXIST?: %s", fullFilePath)
+
 		err := os.Mkdir(relativePath, os.ModePerm)
 		if err != nil {
 			log.Fatalf("Error creating file on path %s/%s: %s", relativePath, fileName, err)
@@ -41,12 +52,30 @@ func SaveFile(fileName string, fileContents string, relativePath string) error {
 }
 
 func FileExists(pathWithFilename string) bool {
-	_, err := os.Open(pathWithFilename)
+	// Ensure we are only reading files from our executable and not where the terminal is executing from.
+	execPath, err := os.Executable()
+	if err != nil {
+		log.Fatal(err)
+	}
+	execDirPath := path.Dir(execPath)
+	fullFilePath := filepath.Join(execDirPath, pathWithFilename)
+	slog.Debug("FILE EXIST?: %s", fullFilePath)
+
+	_, err = os.Open(fullFilePath)
 	return !errors.Is(err, os.ErrNotExist)
 }
 
 func ReadFile(pathWithFilename string) *[]byte {
-	file, err := os.ReadFile(pathWithFilename)
+	// Ensure we are only reading files from our executable and not where the terminal is executing from.
+	execPath, err := os.Executable()
+	if err != nil {
+		log.Fatal(err)
+	}
+	execDirPath := path.Dir(execPath)
+	fullFilePath := filepath.Join(execDirPath, pathWithFilename)
+	slog.Debug("READING FILE: %s", fullFilePath)
+
+	file, err := os.ReadFile(fullFilePath)
 	if !errors.Is(err, os.ErrNotExist) {
 		return &file
 	}
