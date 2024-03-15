58ed9ff04798ae291816995fdf1d69fe4a1dc63f
Dawson Hensel <dawsonhensel@gmail.com>
2024-03-15 09:08:26 -0700

    Compile properly for windows and fix signature of only the binary for macos

--- build/windows_amd64.sh
+++ build/windows_amd64.sh
@@ -1,4 +1,4 @@
-GOARCH=amd64 GOOS=windows go build -o release/Drawbridge_windows_amd64_$1.exe -ldflags="-s -w" .
+CGO_ENABLED=1 CC="x86_64-w64-mingw32-gcc" GOARCH=amd64 GOOS=windows go build -o release/Drawbridge_windows_amd64_$1.exe -ldflags="-s -w" .
 cp -R ./cmd/dashboard/ui/static ./release/
 tar -zcvf ./release/Drawbridge_windows_amd64_$1.zip release/static/ release/Drawbridge_windows_amd64_$1.exe
-gpg --output release/Drawbridge_windows_amd64_$1.zip.sig --detach-sig release/Drawbridge_windows_amd64_$1.exe
+gpg --output release/Drawbridge_windows_amd64_$1.zip.sig --detach-sig release/Drawbridge_windows_amd64_$1.zip
