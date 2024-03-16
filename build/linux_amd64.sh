a138a2f7339a46e7eca8b2942f8fc01a74bbb529
Dawson Hensel <dawsonhensel@gmail.com>
2024-03-15 20:33:26 -0700

    dont use tar as it doesnt unzip on windows

--- build/linux_amd64.sh
+++ build/linux_amd64.sh
@@ -1,4 +1,4 @@
 GOARCH=amd64 GOOS=linux go build -o release/Drawbridge_linux_amd64_$1 -ldflags="-s -w" .
 cp -R ./cmd/dashboard/ui/static ./release/
-tar -zcvf ./release/Drawbridge_linux_amd64_$1.zip release/static/ release/Drawbridge_linux_amd64_$1
+zip -r ./release/Drawbridge_linux_amd64_$1.zip release/static/ release/Drawbridge_linux_amd64_$1
 gpg --output release/Drawbridge_linux_amd64_$1.zip.sig --detach-sig release/Drawbridge_linux_amd64_$1.zip
