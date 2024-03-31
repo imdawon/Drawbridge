49871f98563c8d9c2a895c8f33c7ad4593d2c45e
Dawson Hensel <dawsonhensel@gmail.com>
2024-03-30 22:57:12 -0700

    make uuid

--- cmd/utils/utils.go
+++ cmd/utils/utils.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"archive/zip"
+	"crypto/rand"
 	"errors"
 	"fmt"
 	"io"
@@ -332,4 +333,17 @@ func CreateDrawbridgeFilePath(relativePathWithFilename string) string {
 
 func PadWithZeros(num int) string {
 	return fmt.Sprintf("%03d", num)
+}
+
+func NewUUID() (string, error) {
+	b := make([]byte, 16)
+	_, err := rand.Read(b)
+	if err != nil {
+		return "", err
+	}
+
+	uuid := fmt.Sprintf("%x-%x-%x-%x-%x",
+		b[0:4], b[4:6], b[6:8], b[8:10], b[10:])
+
+	return uuid, nil
 }
